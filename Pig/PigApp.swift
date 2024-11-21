c5aae1facd41f25a5a9d2dc651c4122daaec101a
Delaney Blaszinski <Dablasz6@gmail.com>
2024-11-21 09:45:13 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Delaney Blaszinski on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
